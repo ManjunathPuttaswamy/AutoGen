4d0b68c740faeb5c97b2225b1239d836eafde1bc
Manjunath Puttaswamy <Manju_86@Outlook.com>
2025-07-26 11:11:33 +0530

    changes were Commited

---
+++ 7. Autogen Projects/1. Analyser GPT/demo_autogen_project.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from autogen_agentchat.conditions import TextMentionTermination
+from autogen_agentchat.teams import RoundRobinGroupChat
+from autogen_agentchat.ui import Console
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+from agents.agents import planner_agent, local_agent, language_agent, travel_summary_agent
+
+async def main():
+    model_client = OpenAIChatCompletionClient(model="gpt-4o")
+
+    termination = TextMentionTermination("TERMINATE")
+    group_chat = RoundRobinGroupChat(
+        [planner_agent, local_agent, language_agent, travel_summary_agent],
+        termination_condition=termination
+    )
+
+    await Console(group_chat.run_stream(task="Plan a 3 day trip to Nepal."))
+    await model_client.close()
+
+# ✅ Run the async main function
+asyncio.run(main())
